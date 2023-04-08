edeb1dc2b846d5c47fb8e28966d6767b8b72d628
Iano Maciel <ianomaciel6385@gmail.com>
2023-04-08 00:04:08 -0400

    feat(#1): component aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,13 +1,25 @@
 import React from "react"
 
+// styles
 import * as S from './styles'
 
-const Aside = () => {
+// icons 
+import {
+    MdPerson,
+    MdOutlineCode,
+    MdOutlineCoPresent
+} from 'react-icons/md'
+
+const Aside = () =>{
     return(
         <S.Container>
-            <h1>Aside</h1>
+            <S.MenuContainer>
+                <S.MenuItemLink href="#"><MdPerson/>Sobre</S.MenuItemLink>
+                <S.MenuItemLink href="#"><MdOutlineCode/>Projetos</S.MenuItemLink>
+                <S.MenuItemLink href="#"><MdOutlineCoPresent/>Palestras</S.MenuItemLink>
+            </S.MenuContainer>
         </S.Container>
     )
-} 
+}
 
 export default Aside
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,5 +1,43 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    grid-area: AS; 
+    width: 100%;
+    height: 100vh;
+
+    grid-area: AS;
+    background-color: rgba(0, 0, 0, 0.3);
+    border-right: 1px solid #D9D9D9;
+    
+`
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin: 80% 20px;
+`
+
+export const MenuItemLink = styled.a`
+    display: flex;
+    align-items: center;
+
+    padding: 10px;
+
+    font-weight: 400;
+    font-size: 26px;
+    color: #FFFFFF;
+    
+    text-decoration: none;
+    transition: opacity .3s;
+
+    &:hover {
+        opacity: .7;
+        background-color: rgba(255, 255, 255, 0.1);
+        color: #FEBC2E;
+        border-radius: 30px;
+    }
+
+    > svg {
+        margin-right: 10px;
+        font-size: 26px;
+    } 
 `
