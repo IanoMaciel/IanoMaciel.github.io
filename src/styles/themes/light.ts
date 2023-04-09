26cebf618e2430d955ce4852002b014fb70fdf0e
Iano Maciel <ianomaciel6385@gmail.com>
2023-04-08 20:45:47 -0400

    feat(#1): light theme creation

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#FFFFFF',
+        secondary: '#1F1F1F',
+        tertiary: '#000000',
+
+        gray: '#DCDCDC',
+    },
+}
