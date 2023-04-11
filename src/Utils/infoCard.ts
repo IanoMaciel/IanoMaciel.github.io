bff9871d6625e6fecf40666a35c911d5d57d47e1
Iano Maciel <ianomaciel6385@gmail.com>
2023-04-11 09:52:13 -0400

    docs(#2): utils

---
+++ src/Utils/infoCard.ts
@@ -0,0 +1,29 @@
+// imagens
+import myWallet from '../assets/image-my-wallet.svg'
+import toDo from '../assets/image-to-do.svg'
+import ImageUni from '../assets/image-uni.svg'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        'title': 'My Wallet',
+        'description': "A web system that monitors user's income and expenses, providing a dashboard for better data visualization. The goal of this system is to simulate a real wallet, allowing the user to easily view their earnings and expenses.",
+        'tech': 'NodeJS, React, and TypeScript',
+        'url': 'https://github.com/IanoMaciel/my-wallet',
+        'image': myWallet,
+    },
+    {
+        'title': 'To Do',
+        'description': "It is a tool that seeks to help the daily tasks of users, which is possible to schedule tasks and notify late tasks. The application is available for both web and mobile devices.",
+        'tech': 'NodeJS, React, React Native, Rest API, and MongoDB.',
+        'url': 'https://github.com/IanoMaciel/ToDo',
+        'image': toDo,
+    },
+    {
+        'title': 'Uni+',
+        'description': "The purpose of this product is to offer a solution for submitting hours related to Academic-Scientific-Cultural Activities (AACC), facilitating the sending of documents, in addition to allowing the student to consult which modality he needs to complete to fill in his hours.",
+        'tech': 'NodeJS, React, JavaScript.',
+        'url': 'https://github.com/IanoMaciel/front-end-aacc',
+        'image': ImageUni,
+    },
+]
